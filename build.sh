03f56d7b4be15b480fb4f0fa40f9be31684a0e40
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-06-27 10:39:19 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_ARM_PREFIX=arm-linux-gnueabi- build -s -n 0 -a ARM -t GCC5 -p MSM8909Pkg/Devices/y560.dsc
 gzip -c < workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd >MSM8909_UEFI.fd.gz
-mkbootimg --kernel MSM8909_UEFI.fd.gz --dt device_specific/huawei-y560.dtb --ramdisk workspace/empty --base 0x80000000 --pagesize 2048 -o uefi.img
+skales-mkbootimg --kernel MSM8909_UEFI.fd.gz --dt device_specific/huawei-y560.dtb --ramdisk workspace/empty --base 0x80000000 --pagesize 2048 -o uefi.img
