033ad798479ef4a7e1c885afb0ae18315753d89a
sonic011gamer <58566108+sonic011gamer@users.noreply.github.com>
2022-06-27 10:30:56 +0300

    Try to make it boot without lk2nd

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ set -e
 . build_common.sh
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
 GCC5_ARM_PREFIX=arm-linux-gnueabi- build -s -n 0 -a ARM -t GCC5 -p MSM8909Pkg/Devices/y560.dsc
-gzip -c < workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd >uefi.img
-cat device_specific/huawei-y560.dtb >>uefi.img
+gzip -c < workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd >MSM8909_UEFI.fd.gz
+cat MSM8909_UEFI.fd.gz device_specific/huawei-y560.dtb > zImage-dtb
+mkbootimg --kernel zImage-dtb --ramdisk workspace/empty --base 0x80000000 --pagesize 2048 -o uefi.img
