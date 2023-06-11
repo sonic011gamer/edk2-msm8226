dec7572bdc3f6cc8124190d3dcc884a752ffa3b7
sonic011gamer <efimgirjeu@gmail.com>
2023-06-11 22:04:08 +0300

    Allow for the stock bootloader to load the bootimg

--- scripts/e1m.sh
+++ scripts/e1m.sh
@@ -7,4 +7,5 @@ GCC5_ARM_PREFIX=arm-linux-gnueabi- build -j$(nproc) -s -n 0 -a ARM -t GCC5 -p MS
 ./scripts/build_bootshim.sh
 cat BootShim/BootShim.bin workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd > workspace/bootpayload.bin
 gzip -c < workspace/bootpayload.bin > workspace/MSM8909_UEFI.fd.gz
-mkbootimg --kernel workspace/MSM8909_UEFI.fd.gz --kernel_offset 0x00008000 --dtb device_specific/saana.dtb --ramdisk workspace/empty --base 0x80000000  --pagesize 2048 --cmdline "" --output workspace/E1M.img
+cat workspace/MSM8909_UEFI.fd.gz device_specific/e1m.dtb > workspace/e1m-kernel
+./scripts/mkbootimg --kernel=workspace/e1m-kernel --output=workspace/E1M.img --cmdline="EDK2" --base=0x80000000
--- scripts/y560.sh
+++ scripts/y560.sh
@@ -6,5 +6,4 @@ set -e
 GCC5_ARM_PREFIX=arm-linux-gnueabi- build -j$(nproc) -s -n 0 -a ARM -t GCC5 -p MSM8909Pkg/Devices/y560.dsc
 ./scripts/build_bootshim.sh
 cat BootShim/BootShim.bin workspace/Build/MSM8909Pkg/DEBUG_GCC5/FV/MSM8909PKG_UEFI.fd > workspace/bootpayload.bin
-gzip -c < workspace/bootpayload.bin >MSM8909_UEFI.fd.gz
-mkbootimg --kernel MSM8909_UEFI.fd.gz --kernel_offset 0x00008000 --dtb device_specific/huawei-y560.dtb --ramdisk workspace/empty --base 0x80000000 --pagesize 2048 --cmdline "" --output workspace/Y560.img
+./scripts/mkbootimg --kernel=workspace/bootpayload.bin --output=workspace/Y560.img --cmdline="EDK2" --qcdt=device_specific/y560.dtb --base=0x80000000
